585606332f70a825406d89c4f7480c9e65cddc9d
freedino <sang2222004@gmail.com>
2024-05-27 02:26:17 +0700

    change

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,10 @@
-from django.http import HttpResponse
+# pages/views.py
+from django.views.generic import TemplateView
 
-def home_page_view(request):
-    return HttpResponse("Hello, World!")
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
